893483f46448c1dfe443bb4fdb41640cd53580ca
yaboiishere <mixaildobrev@gmail.com>
2020-07-21 23:12:01 +0300

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Windows.Devices.Sensors;
+
+namespace InclineometerToController {
+    class Program {
+        static void Main(string[] args) {
+            Inclinometer _inclinometer = Inclinometer.GetDefault();
+            if(_inclinometer != null) {
+                // Establish the report interval
+                _inclinometer.ReportInterval = 10;
+                int start = 0;
+                while(start < 1000000) {
+                    Console.WriteLine(_inclinometer.GetCurrentReading());
+                    start++;
+                }
+
+            }
+            else {
+                Console.WriteLine("No inclinometer found");
+            }
+            Console.ReadLine();
+        }
+    }
+}
